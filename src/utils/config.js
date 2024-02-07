87c461aae6cc22d787a1751077bb94fbabca2619
Kellen Bavis <kellenbavis10@gmail.com>
2024-02-07 09:15:25 -0500

    Updating API URL based on updated EC2 instance. Backend will run on seperate ec2 isntance from frontend due to memory consumption limitations of ec2 instance

--- src/utils/config.js
+++ src/utils/config.js
@@ -1,3 +1,3 @@
 //TODO: Consider using seperate methodology
-const apiUrl = "http://ec2-44-201-100-80.compute-1.amazonaws.com:8080";
+const apiUrl = "http://ec2-44-210-93-89.compute-1.amazonaws.com:8080";
 export default apiUrl;
