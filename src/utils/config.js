10c0136c640793ab3655b263af5723e7ada7e579
Kellen Bavis <kellenbavis10@gmail.com>
2024-02-08 09:22:35 -0500

    Updating API URL based on ec2 instance changes

--- src/utils/config.js
+++ src/utils/config.js
@@ -1,3 +1,3 @@
 //TODO: Consider using seperate methodology
-const apiUrl = "http://ec2-44-210-93-89.compute-1.amazonaws.com:8080";
+const apiUrl = "http://ec2-3-86-252-56.compute-1.amazonaws.com:8080";
 export default apiUrl;
